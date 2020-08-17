55a52d079550e232cc068eb703a858cddb7d2d6f
Jerbi-Yasmine <yasmine.jerbi@ensi-uma.tn>
2020-08-17 17:27:55 +0100

    modification bd 1

--- e-learning.sql
+++ e-learning.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  lun. 17 août 2020 à 13:30
+-- Généré le :  lun. 17 août 2020 à 18:26
 -- Version du serveur :  10.1.36-MariaDB
 -- Version de PHP :  7.2.11
 
@@ -40,6 +40,32 @@ CREATE TABLE `cours` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `invitation`
+--
+
+CREATE TABLE `invitation` (
+  `id` int(3) NOT NULL,
+  `etudiant` varchar(50) NOT NULL,
+  `matiere` varchar(50) NOT NULL,
+  `prof` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `matiere`
+--
+
+CREATE TABLE `matiere` (
+  `id` int(2) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prof` varchar(50) NOT NULL,
+  `acces` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `personne`
 --
 
@@ -62,7 +88,16 @@ INSERT INTO `personne` (`id`, `email`, `mdp`, `nom`, `statut`, `matiere`) VALUES
 (3, 'tarak3072@yahoo.fr', '11', 'jerbi', 1, ''),
 (4, 'ytre@ihlg.g', 'a', 'ddddddd', 2, ''),
 (5, 'yt@gmail.yn', '147852369', 'hhh', 2, ''),
-(6, 'imed@gmail.com', '1245789663', 'imed', 1, 'oriente objet');
+(6, 'imed@gmail.com', '1245789663', 'imed', 1, 'oriente objet'),
+(7, 'amina@gmail.com', '14523698', 'amina', 2, ''),
+(8, 'amir@yahoo.fr', '145236987', 'amir', 2, ''),
+(9, 'dlkcl@krf.gg', '145236987', 'dddddddlkij', 1, ''),
+(10, 'dlkcl@krf.ggt', 'mpoiuyt5555', 'hjk', 1, ''),
+(11, 'a@gmail.com', '145236987', 'aaa', 2, ''),
+(12, 'offfffffflh@llh.hg', '14523698785', 'b', 1, ''),
+(13, 'amina@gmail.comk', '785412369', 'aaalkj', 1, ''),
+(14, 'dd@kfkf.ffd', '145236987', 'e', 2, ''),
+(15, 'gg@jj.hh', '147852369', 'gg', 1, '');
 
 --
 -- Index pour les tables déchargées
@@ -72,6 +107,18 @@ INSERT INTO `personne` (`id`, `email`, `mdp`, `nom`, `statut`, `matiere`) VALUES
 -- Index pour la table `cours`
 --
 ALTER TABLE `cours`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `invitation`
+--
+ALTER TABLE `invitation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `matiere`
+--
+ALTER TABLE `matiere`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -91,10 +138,22 @@ ALTER TABLE `cours`
   MODIFY `id` int(15) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT pour la table `invitation`
+--
+ALTER TABLE `invitation`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `matiere`
+--
+ALTER TABLE `matiere`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `personne`
 --
 ALTER TABLE `personne`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
