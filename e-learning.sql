3cfe858ab70a13d7df6134cf7978012eacb2d2b3
Jerbi-Yasmine <yasmine.jerbi@ensi-uma.tn>
2020-08-17 12:31:56 +0100

    ajout bd 1

---
+++ e-learning.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  lun. 17 août 2020 à 13:30
+-- Version du serveur :  10.1.36-MariaDB
+-- Version de PHP :  7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `e-learning`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cours`
+--
+
+CREATE TABLE `cours` (
+  `id` int(15) NOT NULL,
+  `matiere` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `acces` int(2) NOT NULL,
+  `nom` varchar(30) NOT NULL,
+  `url` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `personne`
+--
+
+CREATE TABLE `personne` (
+  `id` int(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mdp` varchar(30) NOT NULL,
+  `nom` varchar(30) NOT NULL,
+  `statut` int(2) NOT NULL,
+  `matiere` varchar(3000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `personne`
+--
+
+INSERT INTO `personne` (`id`, `email`, `mdp`, `nom`, `statut`, `matiere`) VALUES
+(1, 'yasmine.jerbi@ensi-uma.tn', '1234567', 'yasmine', 2, ''),
+(2, 'firas@gmail.com', '14', 'firas', 2, ''),
+(3, 'tarak3072@yahoo.fr', '11', 'jerbi', 1, ''),
+(4, 'ytre@ihlg.g', 'a', 'ddddddd', 2, ''),
+(5, 'yt@gmail.yn', '147852369', 'hhh', 2, ''),
+(6, 'imed@gmail.com', '1245789663', 'imed', 1, 'oriente objet');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `cours`
+--
+ALTER TABLE `cours`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `personne`
+--
+ALTER TABLE `personne`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `cours`
+--
+ALTER TABLE `cours`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `personne`
+--
+ALTER TABLE `personne`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
